b42a87052ed4a3c80a3a050d7949226f4e2d5ee4
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-01-17 03:34:26 +0700

    Update nekobin.py

--- Geez/modules/basic/nekobin.py
+++ Geez/modules/basic/nekobin.py
@@ -90,7 +90,7 @@ async def get_pastebin_service(text: str):
         pastebin = "spacebin"
     return pastebin
 
-@Client.on_message(filters.command(["paste", "nekobin", "hastebin", "spacebin"], [cmd]) & filters.me)
+@Client.on_message(filters.command(["paste", "nekobin", "hastebin", "spacebin"], cmd) & filters.me)
 async def paste_dis_text(_, message: Message):
     pstbin_serv = await get_pastebin_service(message.text.split(" ")[0])
     paste_msg = await message.edit(f"`Pasting to {pstbin_serv.capitalize()}...`")

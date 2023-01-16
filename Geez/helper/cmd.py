f3ac7c2db495dd180ca8d8f3e18ee6aa8121ffd9
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-01-17 03:25:56 +0700

    Create cmd.py

---
+++ Geez/helper/cmd.py
@@ -0,0 +1,9 @@
+# Create Modules By @xtsea
+from pyrogram import *
+from pyrogram import Client as ren
+
+cmd = ["^", "?", "-", "+"] # cmd custom
+
+command = filters.command
+
+dont_know = -1001640257827

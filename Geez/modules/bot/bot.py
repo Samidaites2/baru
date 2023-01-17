72dd0deb49e35b748769d9d44de4b5c9bcfae333
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-01-17 09:10:13 +0700

    Update bot.py

--- Geez/modules/bot/bot.py
+++ Geez/modules/bot/bot.py
@@ -49,7 +49,6 @@ async def on_plug_in_cb(_, callback_query: CallbackQuery):
     await app.edit_inline_text(
         callback_query.inline_message_id,
         reply_pop_up_alert,
-        reply_markup=InlineKeyboardMarkup(buttons),
     )
 
 

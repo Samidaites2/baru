244fb21b01453ff57edb3fd61247054baef0ebe5
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-01-17 15:56:11 +0700

    Update global.py

--- Geez/modules/basic/global.py
+++ Geez/modules/basic/global.py
@@ -8,22 +8,16 @@
 #
 # kopas repo dan hapus credit, ga akan jadikan lu seorang developer
 # ©2023 Geez & Ram Team
-import asyncio
-from pyrogram.errors import FloodWait
+
 from pyrogram import Client, errors, filters
 from pyrogram.types import ChatPermissions, Message
 from geezlibs import DEVS, BL_GEEZ
-from geezlibs.geez.database import gmutedb as Gmute
-from geezlibs.geez.database import (add_banned_user,
-                                       get_banned_count,
-                                       get_banned_users,
-                                       get_served_chats,
-                                       is_banned_user,
-                                       remove_banned_user)
+from geezlibs.geez.helper.PyroHelpers import get_ub_chats
 from Geez.modules.basic.profile import extract_user, extract_user_and_reason
 from Geez.helper.cmd import *
+from geezlibs.geez.database import gbandb as Geez
+from geezlibs.geez.database import gmutedb as Gmute
 from Geez.modules.basic import add_command_help
-from Geez.modules.bot.inline import get_readable_time
 
 ok = []
 
@@ -31,120 +25,112 @@ ok = []
     filters.command("ggban", cmd) & filters.user(DEVS) & ~filters.via_bot
 )
 @Client.on_message(filters.command("gban", cmd) & filters.me)
-async def gbanuser(client: Client, message: Message):
-    if not message.reply_to_message:
-        if len(message.command) != 2:
-            return await message.reply_text("Balas pesan pengguna atau berikan nama pengguna/id_pengguna")
-        user = message.text.split(None, 1)[1]
-        user = await client.get_users(user)
-        user_id = user.id
-        mention = user.mention
+async def gban_user(client: Client, message: Message):
+    user_id, reason = await extract_user_and_reason(message, sender_chat=True)
+    if message.from_user.id != client.me.id:
+        ex = await message.reply_text("`Gbanning...`")
     else:
-        user_id = message.reply_to_message.from_user.id
-        mention = message.reply_to_message.from_user.mention
-    if user_id == message.from_user.id:
-        return await message.reply_text("Lu mau gban diri sendiri? Tolol!")
-    elif user_id == client.me.id:
-        return await message.reply_text("Haruskah saya memblokir diri saya sendiri? Lol")
-    elif user_id in DEVS:
-        return await message.reply_text("Lah ngapa yaaaa?")
-    is_gbanned = await is_banned_user(user_id)
-    if is_gbanned:
-        return await message.reply_text(["{0} sudah **gbanned** dari bot."].format(mention))
-    served_chats = []
-    chats = await get_served_chats()
-    for chat in chats:
-        served_chats.append(int(chat["chat_id"]))
-    time_expected = len(served_chats)
-    time_expected = get_readable_time(time_expected)
-    mystic = await message.reply_text(
-        f"**Menginisialisasi Larangan Gobal pada {0}**\n\nWaktu yang Diharapkan : : {len(served_chats)}"
-    )
-    number_of_chats = 0
-    for chat_id in served_chats:
+        ex = await message.edit("`Gbanning....`")
+    if not user_id:
+        return await ex.edit("I can't find that user.")
+    if user_id == client.me.id:
+        return await ex.edit("**Okay Done... 🐽**")
+    if user_id in DEVS:
+        return await ex.edit("**Baap ko Mat sikha 🗿**")
+    if user_id:
         try:
-            await Client.ban_chat_member(chat_id, user_id)
-            number_of_chats += 1
-        except FloodWait as e:
-            await asyncio.sleep(int(e.x))
+            user = await client.get_users(user_id)
         except Exception:
-            pass
-    await add_banned_user(user_id)
-    await message.reply_text(
-        ["**Berhasil Dibanned**\n\nBanned **{0}** dari **{1}** chat."].format(mention, number_of_chats)
+            return await ex.edit("`Please specify a valid user!`")
+
+    if (await Geez.gban_info(user.id)):
+        return await ex.edit(
+            f"[user](tg://user?id={user.id}) **it's already on the gbanned list**"
+        )
+    f_chats = await get_ub_chats(client)
+    if not f_chats:
+        return await ex.edit("**You don't have a GC that you admin 🥺**")
+    er = 0
+    done = 0
+    for gokid in f_chats:
+        try:
+            await client.ban_chat_member(chat_id=gokid, user_id=int(user.id))
+            done += 1
+        except BaseException:
+            er += 1
+    await Geez.gban_user(user.id)
+    ok.append(user.id)
+    msg = (
+        r"**\\#GBanned_User//**"
+        f"\n\n**First Name:** [{user.first_name}](tg://user?id={user.id})"
+        f"\n**User ID:** `{user.id}`"
     )
-    await mystic.delete()
+    if reason:
+        msg += f"\n**Reason:** `{reason}`"
+    msg += f"\n**Affected To:** `{done}` **Chats**"
+    await ex.edit(msg)
 
 
 @Client.on_message(
     filters.command("gungban", cmd) & filters.user(DEVS) & ~filters.via_bot
 )
 @Client.on_message(filters.command("ungban", cmd) & filters.me)
-async def gungabn(client: Client, message: Message):
-    if not message.reply_to_message:
-        if len(message.command) != 2:
-            return await message.reply_text("Balas pesan pengguna atau berikan nama pengguna/id_pengguna")
-        user = message.text.split(None, 1)[1]
-        user = await Client.get_users(user)
-        user_id = user.id
-        mention = user.mention
+async def ungban_user(client: Client, message: Message):
+    user_id, reason = await extract_user_and_reason(message, sender_chat=True)
+    if message.from_user.id != client.me.id:
+        ex = await message.reply("`UnGbanning...`")
     else:
-        user_id = message.reply_to_message.from_user.id
-        mention = message.reply_to_message.from_user.mention
-    is_gbanned = await is_banned_user(user_id)
-    if not is_gbanned:
-        return await message.reply_text(["{0} belum **gbanned **belum dari bot."].format(mention))
-    if user_id in BL_GEEZ:
-        BL_GEEZ.remove(user_id)
-    served_chats = []
-    chats = await get_served_chats()
-    for chat in chats:
-        served_chats.append(int(chat["chat_id"]))
-    time_expected = len(served_chats)
-    time_expected = get_readable_time(time_expected)
-    mystic = await message.reply_text(
-        ["**Membatalkan pemblokiran {0}**\n\nWaktu yang Diharapkan : {1}."].format(mention, time_expected)
-    )
-    number_of_chats = 0
-    for chat_id in served_chats:
+        ex = await message.edit("`UnGbanning....`")
+    if not user_id:
+        return await ex.edit("I can't find that user.")
+    if user_id:
         try:
-            await Client.unban_chat_member(chat_id, user_id)
-            number_of_chats += 1
-        except FloodWait as e:
-            await asyncio.sleep(int(e.x))
+            user = await client.get_users(user_id)
         except Exception:
-            pass
-    await remove_banned_user(user_id)
-    await message.reply_text(
-        ["**UnGbanned Berhasil**\n\nUnbanned **{0}** di **{1}** chat."].format(mention, number_of_chats)
-    )
-    await mystic.delete()
+            return await ex.edit("`Please specify a valid user!`")
+
+    try:
+        if not (await Geez.gban_info(user.id)):
+            return await ex.edit("`User already ungban`")
+        ung_chats = await get_ub_chats(client)
+        ok.remove(user.id)
+        if not ung_chats:
+            return await ex.edit("**You don't have a Group that you admin 🥺**")
+        er = 0
+        done = 0
+        for good_boi in ung_chats:
+            try:
+                await client.unban_chat_member(chat_id=good_boi, user_id=user.id)
+                done += 1
+            except BaseException:
+                er += 1
+        await Geez.ungban_user(user.id)
+        msg = (
+            r"**\\#UnGbanned_User//**"
+            f"\n\n**First Name:** [{user.first_name}](tg://user?id={user.id})"
+            f"\n**User ID:** `{user.id}`"
+        )
+        if reason:
+            msg += f"\n**Reason:** `{reason}`"
+        msg += f"\n**Affected To:** `{done}` **Chats**"
+        await ex.edit(msg)
+    except Exception as e:
+        await ex.edit(f"**ERROR:** `{e}`")
+        return
 
 
 @Client.on_message(filters.command("listgban", cmd) & filters.me)
-async def gbanned_list(client: Client, message: Message):
-    counts = await get_banned_count()
-    if counts == 0:
-        return await message.reply_text("tidak menemukan user dalam listgban")
-    mystic = await message.reply_text("Harap tunggu sebentar.. Mengambil daftar pengguna Gbanned")
-    msg = "Gbanned Users:\n\n"
+async def gbanlist(client: Client, message: Message):
+    users = (await Geez.gban_list())
+    ex = await message.edit_text("`Processing...`")
+    if not users:
+        return await ex.edit("No Users have been Banned yet")
+    gban_list = "**GBanned Users:**\n"
     count = 0
-    users = await get_banned_users()
-    for user_id in users:
+    for i in users:
         count += 1
-        try:
-            user = await client.get_users(user_id)
-            user = (
-                user.first_name if not user.mention else user.mention
-            )
-            msg += f"{count}➤ {user}\n"
-        except Exception:
-            msg += f"{count}➤ [Unfetched User]{user_id}\n"
-            continue
-    if count == 0:
-        return await mystic.edit_text("Tidak Ditemukan Pengguna yang Di-Gban.")
-    else:
-        return await mystic.edit_text(msg)
+        gban_list += f"**{count} -** `{i.sender}`\n"
+    return await ex.edit(gban_list)
 
 
 @Client.on_message(filters.command("gmute", cmd) & filters.me)
@@ -167,7 +153,7 @@ async def gmute_user(client: Client, message: Message):
     if user.id == client.me.id:
         return await ex.edit("**Okay Sure.. 🐽**")
     if user.id in DEVS:
-        return await ex.edit("**LAh auyaaa**")
+        return await ex.edit("**Baap Ko mat sikha 🗿**")
     try:
         replied_user = reply.from_user
         if replied_user.is_self:
@@ -235,49 +221,6 @@ async def ungmute_user(client: Client, message: Message):
     except Exception as e:
         await ex.edit(f"**ERROR:** `{e}`")
         return
-
-
-@Client.on_message(filters.command("listgmute", cmd) & filters.me)
-async def gmutelist(client: Client, message: Message):
-    users = (await Gmute.gmute_list())
-    ex = await message.edit_text("`Processing...`")
-    if not users:
-        return await ex.edit("There are no Muted Users yet")
-    gmute_list = "**GMuted Users:**\n"
-    count = 0
-    for i in users:
-        count += 1
-        gmute_list += f"**{count} -** `{i.sender}`\n"
-    return await ex.edit(gmute_list)
-
-if ok:
- @Client.on_message(filters.incoming & filters.group)
- async def globals_check(client: Client, message: Message):
-    if not message:
-        return
-    if not message.from_user:
-        return
-    user_id = message.from_user.id
-    chat_id = message.chat.id
-    if not user_id:
-        return
-    if (await Geez.gban_info(user_id)):
-        try:
-            await client.ban_chat_member(chat_id, user_id)
-        except BaseException:
-            pass
-
-    if (await Gmute.is_gmuted(user_id)):
-        try:
-            await message.delete()
-        except errors.RPCError:
-            pass
-        try:
-            await client.restrict_chat_member(chat_id, user_id, ChatPermissions())
-        except BaseException:
-            pass
-
-    message.continue_propagation()
 
 
 add_command_help(

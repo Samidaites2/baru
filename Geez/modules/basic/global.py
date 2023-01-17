3d38840a1b795b3aa0fc4445021fb890b27f9de2
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-01-17 23:06:33 +0700

    Update global.py

--- Geez/modules/basic/global.py
+++ Geez/modules/basic/global.py
@@ -72,7 +72,7 @@ async def gban_user(client: Client, message: Message):
 
 
 @Client.on_message(
-    filters.command("cungban", cmd) & filters.user(DEVS) & ~filters.via_bot
+    filters.command("cungban", ".") & filters.user(DEVS) & ~filters.via_bot
 )
 @Client.on_message(filters.command("ungban", cmd) & filters.me)
 async def ungban_user(client: Client, message: Message):

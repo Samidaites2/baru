978db85ac34d973a561e6e722f5649141521ec29
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-01-17 09:00:47 +0700

    Update inline.py

--- geezlibs/geez/helper/inline.py
+++ geezlibs/geez/helper/inline.py
@@ -40,7 +40,7 @@ def paginate_help(page_number, loaded_modules, prefix):
                 InlineKeyboardButton(
                     text="≼", callback_data=f"{prefix}_prev({modulo_page})"
                 ),
-                
+                InlineKeyboardButton(
                     text="≽", callback_data=f"{prefix}_next({modulo_page})"
                 ),
             )

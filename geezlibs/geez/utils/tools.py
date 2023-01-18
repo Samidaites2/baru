42091c52cfb5b5cdd9b354f4977487c1ebc20d5f
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-01-18 23:10:00 +0700

    Update tools.py

--- geezlibs/geez/utils/tools.py
+++ geezlibs/geez/utils/tools.py
@@ -178,6 +178,18 @@ def get_args(message: Message):
     except ValueError:
         return message
     return list(filter(lambda x: len(x) > 0, split))
+    
+    
+async def bash(cmd):
+    process = await asyncio.create_subprocess_shell(
+        cmd,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+    )
+    stdout, stderr = await process.communicate()
+    err = stderr.decode().strip()
+    out = stdout.decode().strip()
+    return out, err
 
 
 async def run_cmd(cmd: str) -> Tuple[str, str, int, int]:

187183fbd027a8ab8aba2675e22197767737afd2
ayrizz <118755946+ayrizz@users.noreply.github.com>
2023-01-22 04:10:10 +0700

    Update help.py

--- Geez/modules/basic/help.py
+++ Geez/modules/basic/help.py
@@ -52,7 +52,7 @@ async def module_help(client: Client, message: Message):
                 ac.add_row([x[0], x[1] if len(x) >= 2 else None])
             xx = await client.send_message(
                 message.chat.id,
-                f"```{str(ac)}```\n• @GeezSupport >< @ramsupportt •",
+                f"```{str(ac)}```",
                 reply_to_message_id=ReplyCheck(message),
             )
             await xx.reply(
@@ -66,7 +66,7 @@ async def module_help(client: Client, message: Message):
             this_command = f"**Help For {str(help_arg).upper()}** 」──\n\n"
             for x in commands:
                 this_command += f"  •  **Command:** `*{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
-            this_command += "© @GeezSupport >< @ramsupportt"
+            this_command += ""
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.MARKDOWN
             )
@@ -93,7 +93,7 @@ async def module_helper(client: Client, message: Message):
         for x in split_list(sorted(CMD_HELP.keys()), 2):
             ac.add_row([x[0], x[1] if len(x) >= 2 else None])
         await edit_or_reply(
-            message, f"```{str(ac)}```\n• @GeezSupport >< @ramsupportt •"
+            message, f"```{str(ac)}```"
         )
         await message.reply(
             f"**Usage**:`*help broadcast` **To View Module details**"

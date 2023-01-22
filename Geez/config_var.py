ea6f4ccbb6b5efd54be4027a93d1c94e8ddce768
ayrizz <118755946+ayrizz@users.noreply.github.com>
2023-01-22 10:29:55 +0700

    Update config_var.py

--- Geez/config_var.py
+++ Geez/config_var.py
@@ -17,7 +17,7 @@ class Config((object)):
     PM_LOGGER = getenv("PM_LOGGER")
     BOTLOG_CHATID = int(getenv("BOTLOG_CHATID") or 0)
     GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
-    REPO_URL = getenv("REPO_URL", "https://github.com/hitokizzy/Geez-Pyro")
+    REPO_URL = getenv("REPO_URL", "https://github.com/ayrizz/baru")
     HEROKU_API_KEY = getenv("HEROKU_API_KEY")
     HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
     BRANCH = getenv("BRANCH", "main") #don't change

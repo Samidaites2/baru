b6c7e1f77dde3994857bf9499abe3665c9cfb7d7
ayrizz <118755946+ayrizz@users.noreply.github.com>
2023-01-23 21:03:28 +0700

    Update inline.py

--- Geez/modules/bot/inline.py
+++ Geez/modules/bot/inline.py
@@ -60,9 +60,8 @@ async def get_readable_time(seconds: int) -> str:
 
 async def alive_function(message: Message, answers):
     uptime = await get_readable_time((time.time() - StartTime))
-    msg = f"""
-<b> — Hey, I am alive.</b>
-
+    msg = f"""**
+<b>Premium Userbot.</b>
 <b> • User :</b> {message.from_user.mention}
 <b> • Plugins :</b> <code>{len(CMD_HELP)} Modules</code>
 <b> • Python Version :</b> <code>{pyver.split()[0]}</code>
@@ -70,7 +69,7 @@ async def alive_function(message: Message, answers):
 <b> • Bot Uptime :</b> <code>{uptime}</code>
 
 <b> — Bot version: {BOT_VER}</b>
-"""
+**"""
     answers.append(
         InlineQueryResultArticle(
             title="Alive",

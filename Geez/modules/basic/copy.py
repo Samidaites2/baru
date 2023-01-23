103a4b2e9ce5f3805d296ab0bbc9347c48637403
ayrizz <118755946+ayrizz@users.noreply.github.com>
2023-01-23 18:17:20 +0700

    Update and rename copy to copy.py

---
+++ Geez/modules/basic/copy.py
@@ -0,0 +1,32 @@
+import asyncio
+import os
+from pyrogram.types import *
+from pyrogram import *
+from pyrogram import Client as gez
+from pyrogram import Client
+from Geez.helper.cmd import *
+from geezlibs.geez.helper.basic import *
+from geezlibs.geez.helper.PyroHelpers import *
+from geezlibs.geez.utils.misc import *
+from geezlibs.geez.utils.tools import *
+
+
+@gez.on_message(filters.command("copy", cmd) & filters.me)
+async def kangcopy(client: Client, message: Message):
+    link = get_arg(message)
+bot = "kangcopybot"
+if link:
+     try:
+         await client.send_message(bot, link)
+         await asyncio.sleep(2)
+         async for ren in client.get_history(bot, limit=1):
+             await ren.copy(message.chat.id)
+      except:
+          pass
+
+add_command_help(
+    "Curi",
+    [
+        [f"copy <link>", "Nyolong Media/Foto."],
+    ],
+)

95ad6fa3d99310b6487d51117cca658c36618d74
ayrizz <118755946+ayrizz@users.noreply.github.com>
2023-01-23 17:24:24 +0700

    Update heroku.py

--- Geez/modules/basic/heroku.py
+++ Geez/modules/basic/heroku.py
@@ -29,7 +29,7 @@ from pyrogram.types import Message
 
 from config import BOTLOG_CHATID, HEROKU_API_KEY, HEROKU_APP_NAME, BRANCH, REPO_URL
 from config import CMD_HNDLR as cmds
-from geezlibs.geez.helper.cmd import *
+from Geez.helper.cmd import *
 from Geez import *
 from geezlibs.geez import *
 from Geez.modules.basic import add_command_help

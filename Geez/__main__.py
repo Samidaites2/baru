967d11469a03b12d8feaa28939347cc31a255555
ayrizz <118755946+ayrizz@users.noreply.github.com>
2023-01-24 18:39:16 +0700

    Update __main__.py

--- Geez/__main__.py
+++ Geez/__main__.py
@@ -1,39 +1,47 @@
 import importlib
+import time
 from pyrogram import idle
 from uvloop import install
-
-from config import CMD_HNDLR
-from Geez.modules import ALL_MODULES
-from Geez import BOTLOG_CHATID, LOGGER, LOOP, aiosession, bot1, bots, app, ids
 from geezlibs import join
 from geezlibs import BOT_VER, __version__ as gver
+from Geez import BOTLOG_CHATID, LOGGER, LOOP, aiosession, bot1, bots, app, ids
+from config import CMD_HNDLR
+from Geez.modules import ALL_MODULES
+
+
 MSG_ON = """
-╼┅━━━━━━━━━━╍━━━━━━━━━━┅╾
 **Premium Userbot Actived ✅**
 ╼┅━━━━━━━━━━╍━━━━━━━━━━┅╾
+**Userbot Version -** `{}`
+**Ketik** `{}alive` **untuk Mengecheck Bot**
+╼┅━━━━━━━━━━╍━━━━━━━━━━┅╾
 """
+MSG_BOT = (f"**Userbot Assistant**\nis alive...")
+
+
 
 
 async def main():
     await app.start()
-    print("Memulai Premium Userbot..")
-    print("Loading Everything.")
+    LOGGER("✅").info(" Premium Userbot..")
+    LOGGER("✅").info("Loading Everything.")
     for all_module in ALL_MODULES:
         importlib.import_module("Geez.modules" + all_module)
-        print(f"Successfully Imported {all_module} ")
+        LOGGER("✅").info(f"Successfully Imported {all_module} ")
     for bot in bots:
         try:
             await bot.start()
             ex = await bot.get_me()
             await join(bot)
             try:
-                await bot.send_message(BOTLOG_CHATID, MSG_ON)
-            except BaseException:
-                pass
-            print(f"Started as {ex.first_name} | {ex.id} ")
+                await bot.send_message(BOTLOG_CHATID, MSG_ON.format(BOT_VER, CMD_HNDLR))
+                await app.send_message(BOTLOG_CHATID, MSG_BOT)
+            except BaseException as a:
+                LOGGER("✅").warning(f"{a}")
+            LOGGER("✅").info(f"Started as {ex.first_name} | {ex.id} ")
             ids.append(ex.id)
         except Exception as e:
-            print(f"{e}")
+            LOGGER("✅").info(f"{e}")
     await idle()
     await aiosession.close()
 
@@ -42,3 +50,4 @@ if __name__ == "__main__":
     LOGGER("✅").info("Starting Premium Userbot")
     install()
     LOOP.run_until_complete(main())
+
